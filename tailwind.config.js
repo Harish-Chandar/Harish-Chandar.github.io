6f2cf8d624340a152242d9bcd498cf00363eb327
Harish-Chandar <149600195+Harish-Chandar@users.noreply.github.com>
2025-06-25 01:01:14 -0500

    tailwind migrated from cdn to cli compile

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./skills.html", "./projects.html", "./activities.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
